1a6c89083c4a7f5602d2bec1defe985d35ed89e6
John Turner <jturner.usa@gmail.com>
2023-03-17 02:35:36 -0400

    EnumAsMut method names were clashing with EnumAs names
    The method_name variable was being set to the same thing regardless of
    whether we were generating an EnumAsMut or EnumAs method.
    
    Some small changes were made to accomodate the extra paths required to
    set method_name and other variables on the EnumAsMut case.

--- src/enum_as.rs
+++ src/enum_as.rs
@@ -17,18 +17,30 @@ pub(crate) fn expand(target: &DataEnum, mutable: bool) -> TokenStream {
 
 fn expand_method(variant: &Variant, mutable: bool) -> TokenStream {
     let lifetime = Lifetime::new("'unwrap_enum_lifetime", Span::call_site());
-    let ownership = if mutable {
-        Ownership::MutBorrowed(Some(lifetime.clone()))
-    } else {
-        Ownership::Borrowed(Some(lifetime.clone()))
-    };
-    let method_type = common::expand_fields_types_to_tuple(&variant.fields, &ownership);
-    let method_name = format_ident!("as_{}", variant.ident.to_string().to_lowercase());
     let variant_ident = &variant.ident;
     let (destructure, bindings) = common::expand_destructure(&variant.fields);
     let bindings_expression = common::expand_destructuring_bindings(bindings.as_slice());
+    let (method_name, method_type, mut_token) = if mutable {
+        (
+            format_ident!("as_mut_{}", variant.ident.to_string().to_lowercase()),
+            common::expand_fields_types_to_tuple(
+                &variant.fields,
+                Ownership::MutBorrowed(Some(&lifetime)),
+            ),
+            quote! { mut },
+        )
+    } else {
+        (
+            format_ident!("as_{}", variant.ident.to_string().to_lowercase()),
+            common::expand_fields_types_to_tuple(
+                &variant.fields,
+                Ownership::Borrowed(Some(&lifetime)),
+            ),
+            quote! {},
+        )
+    };
     quote! {
-        pub fn #method_name < #lifetime > (& #lifetime self) -> Option< #method_type > {
+        pub fn #method_name < #lifetime > (& #lifetime #mut_token self  ) -> Option< #method_type > {
             match self {
                 Self:: #variant_ident #destructure => Some( #bindings_expression ),
                 _ => None
